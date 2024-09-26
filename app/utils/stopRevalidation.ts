c14dbb17c666604c4a6bec63bcc5787c0d1baf6b
Ethan Chennault <37466516+3than0ls@users.noreply.github.com>
2024-09-26 13:24:18 -0700

    fix: stop revalidation behavior

--- app/utils/stopRevalidation.ts
+++ app/utils/stopRevalidation.ts
@@ -3,9 +3,14 @@ import { ShouldRevalidateFunction } from '@remix-run/react'
 // https://remix.run/docs/en/main/route/should-revalidate
 const stopRevalidate: ShouldRevalidateFunction = ({
     formMethod,
+    currentParams,
+    nextParams,
     defaultShouldRevalidate,
 }) => {
-    if (formMethod === undefined) {
+    if (
+        formMethod === undefined &&
+        currentParams.budgetId === nextParams.budgetId
+    ) {
         return false
     }
     return defaultShouldRevalidate
